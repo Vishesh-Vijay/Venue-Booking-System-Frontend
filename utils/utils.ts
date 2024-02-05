3bbf95f7edf8d597dbc4ec00d82ddadf1d718456
Vishesh Vijayvargiya <vijay.vishu03@gmail.com>
2024-02-05 20:01:24 +0530

    venue added and create venue completed

--- utils/utils.ts
+++ utils/utils.ts
@@ -2,32 +2,55 @@ import axios, { AxiosRequestConfig } from "axios";
 interface userDataProps {
   name: string;
   email: string;
-//   profile_picture: string;
+  //   profile_picture: string;
 }
 
-export const loginUser = async (user_data: userDataProps, credentials: string) => {
-  try {
-        const response = await axios
-            .post("http://127.0.0.1:8000/users/login/", user_data, {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: credentials,
-                },
-            });
-        // Extracting the JSON data from the response
-        
-        // console.log(jsonData,status);
-        return response;
-    } catch (error) {
-        console.error("Error:", error);
+interface addNewVenueProps {
+  name: string;
+  authority_id: string;
+  building_id: string;
+  floor_number: Number;
+  seating_capacity: Number;
+  venue_type: String;
+  is_accessible: Boolean;
+  has_air_conditioner: Boolean;
+  has_projectors: Boolean;
+  has_speakers: Boolean;
+  has_whiteboard: Boolean;
+}
 
-        // throw error;
-    }
+export const loginUser = async (
+  user_data: userDataProps,
+  credentials: string
+) => {
+  try {
+    const response = await axios.post(
+      "http://127.0.0.1:8000/users/login/",
+      user_data,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: credentials,
+        },
+      }
+    );
+    // Extracting the JSON data from the response
+
+    // console.log(jsonData,status);
+    return response;
+  } catch (error) {
+    console.error("Error:", error);
+
+    // throw error;
+  }
 };
 
-export const getUserDetailsByEmail = async (email:string,credentials:string)=>{
-    try {
-       const response = await axios.get(
+export const getUserDetailsByEmail = async (
+  email: string,
+  credentials: string
+) => {
+  try {
+    const response = await axios.get(
       `http://127.0.0.1:8000/users/details/${email}/`,
       {
         headers: {
@@ -36,14 +59,14 @@ export const getUserDetailsByEmail = async (email:string,credentials:string)=>{
         },
       }
     );
-    return response 
-    } catch (error) {
-        console.log(error)
-        // throw error
-    } 
-}
+    return response;
+  } catch (error) {
+    console.log(error);
+    // throw error
+  }
+};
 
-export const getAllBuildings = async (credentials:string) =>{
+export const getAllBuildings = async (credentials: string) => {
   try {
     const response = await axios.get(
       `http://127.0.0.1:8000/buildings/details/all/`,
@@ -58,34 +81,59 @@ export const getAllBuildings = async (credentials:string) =>{
   } catch (error) {
     console.log(error);
     // throw error;
-  }  
-}
+  }
+};
 
-export const addNewBuilding = async(name:string,credentials:string) =>{
- try {
-   const response = await axios.post(
-     `http://127.0.0.1:8000/buildings/add/`,{'name':name},
-     {
-       headers: {
-         "Content-Type": "application/json",
-         Authorization: credentials,
-       },
-     }
-   );
-   return response;
- } catch (error) {
-   console.log(error);
-  //  throw error;
- }   
-}
+export const getBuildingDetailsById = async (
+  building_id: string,
+  credentials: string
+) => {
+  try {
+    const response = await axios.get(
+      `http://127.0.0.1:8000/buildings/details/${building_id}/`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: credentials,
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+    // throw error;
+  }
+};
+export const addNewBuilding = async (name: string, credentials: string) => {
+  try {
+    const response = await axios.post(
+      `http://127.0.0.1:8000/buildings/add/`,
+      { name: name },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: credentials,
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+    //  throw error;
+  }
+};
 
-export const UpdateExistingBuilding = async (id:string,name: string, credentials: string) => {
+export const UpdateExistingBuilding = async (
+  id: string,
+  name: string,
+  credentials: string
+) => {
   try {
     const response = await axios.post(
       `http://127.0.0.1:8000/buildings/update/`,
-      { 
-        id:id,
-        name: name 
+      {
+        id: id,
+        name: name,
       },
       {
         headers: {
@@ -101,7 +149,7 @@ export const UpdateExistingBuilding = async (id:string,name: string, credentials
   }
 };
 
-export const DeleteBuilding = async (id:string,credentials:string)=>{
+export const DeleteBuilding = async (id: string, credentials: string) => {
   try {
     const response = await axios.post(
       `http://127.0.0.1:8000/buildings/remove/`,
@@ -119,5 +167,42 @@ export const DeleteBuilding = async (id:string,credentials:string)=>{
   } catch (error) {
     console.log(error);
     //  throw error;
-  } 
-}
+  }
+};
+
+export const getAllVenues = async (credentials: string) => {
+  try {
+    const response = await axios.get(
+      `http://127.0.0.1:8000/venues/details/all/`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: credentials,
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+    // throw error;
+  }
+};
+
+export const addNewVenue = async (props: addNewVenueProps,credentials:string) => {
+  try {
+    const response = await axios.post(
+      `http://127.0.0.1:8000/venues/add/`,
+      props,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: credentials,
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+    //  throw error;
+  }
+};
